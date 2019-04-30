c2542ae9b6f1a27c4effb0c68a87aa30b658e110
Adam Gipril <adam.gipril@icloud.com>
2019-04-29 18:07:04 -0600

    todo update completed
    Co-authored by Alyssa Wei

--- datastore/index.js
+++ datastore/index.js
@@ -33,12 +33,6 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  // var text = items[id];
-  // if (!text) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   callback(null, { id, text });
-  // }
   fs.readFile(`${exports.dataDir}/${id}.txt`, (err, text) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
@@ -50,13 +44,19 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  exports.readOne(id, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
